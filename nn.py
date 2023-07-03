ba4a0eaf6af62f814d6a6d744173a1eb51bdbc84
Ahmad FB <ahmadbasymeleh@gmail.com>
2023-07-03 19:03:50 +0800

    update path for import nn model

--- nn.py
+++ nn.py
@@ -2,11 +2,11 @@ import pickle
 from sklearn.preprocessing import LabelEncoder
 
 # exported with scikit v1.3.0
-with open('model_nn/labelencoder.pkl', 'rb') as file:
+with open('resource/model_nn/labelencoder.pkl', 'rb') as file:
     le = pickle.load(file)
-with open('model_nn/model.pkl', 'rb') as file:
+with open('resource/model_nn/model.pkl', 'rb') as file:
     model = pickle.load(file)
-with open('model_nn/vectorizer.pkl', 'rb') as file:
+with open('resource/model_nn/vectorizer.pkl', 'rb') as file:
     count_vect = pickle.load(file)
 
 def predict_sentiment(text):
